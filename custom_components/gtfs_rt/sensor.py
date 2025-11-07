182a085fe79038abf102fb852d1fee7df0a32d30
William Mura <williammura@microsoft.com>
2025-11-07 11:57:13 -0800

    complete refactoring

--- custom_components/gtfs_rt/sensor.py
+++ custom_components/gtfs_rt/sensor.py
@@ -85,6 +85,26 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
+async def async_setup_platform(hass, config, add_entities, discovery_info=None):
+    data = PublicTransportData(
+        config.get(CONF_TRIP_UPDATE_URL),
+        config.get(CONF_VEHICLE_POSITION_URL),
+        config.get(CONF_ROUTE_DELIMITER),
+        config.get(CONF_API_KEY),
+        config.get(CONF_API_KEY_HEADER_NAME),
+        config.get(CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL),
+        config.get(CONF_STATIC_GTFS_URL),
+        config.get(CONF_ENABLE_STATIC_FALLBACK, False),
+    )
+
+    # Create sensors
+    sensors = SensorFactory.create_sensors_from_config(config, data)
+    add_entities(sensors)
+
+    # Fetch static GTFS data
+    # hass.create_task(data.load_gtfs_static_data())
+
+
 class SensorFactory:
     """Factory class for creating sensors based on configuration."""
 
